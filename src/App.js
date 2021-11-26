0625121dcd0215272666af024514439ae9a6729f
yaseenbinsaleem <83838667+yaseenbinsaleem@users.noreply.github.com>
2021-11-26 03:06:48 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import Card from "./component/Card";
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import Quiz from "./component/Quiz";
 import Quizapp from "./component/Quizapp";
 
 function App() {
-  const state = useSelector((e) => e);
 
   return (
     <>
-      {/* <Quiz /> */}
-      <Card />
-
-      {/* <Quizapp /> */}
+      <Quizapp /> 
     </>
   );
 }
